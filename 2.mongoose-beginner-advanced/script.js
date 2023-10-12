d0fb5245df08e66e0138cdec11563753586f5c67
Puneet Sivananda <puneet.sivananda@walmart.com>
2023-10-12 11:26:42 +0530

    feat: some query basics

--- 2.mongoose-beginner-advanced/script.js
+++ 2.mongoose-beginner-advanced/script.js
@@ -25,9 +25,11 @@ run()
     console.err(err);
     mongoose.connection.close();
   });
+
 async function run() {
   // Create User
-  const user = await User.create({
+  /**
+    const user = await User.create({
     name: "Kyle",
     age: 30,
     hobbies: ["WeightLifting", "Bowling"],
@@ -36,6 +38,25 @@ async function run() {
     },
     email: "sample@user.com",
   });
+  **/
 
+  // find user by id
+  const user = await User.findById("6527881c84171fb29cb9ad1f");
   console.log(user);
+
+  // find all users that match
+  const kyle = await User.find({ name: "Kyle" });
+  console.log(kyle);
+
+  // find first or one match from collection
+  const findOne = await User.findOne({ name: "Kyle" });
+  console.log(findOne);
+
+  // check if user exists in the db
+  const oneUser = await User.exists({ name: "Kyle" });
+  console.log(oneUser);
+
+  // delete one user
+  const deletedUser = await User.deleteOne({ name: "Kyle" });
+  console.log(deletedUser);
 }
