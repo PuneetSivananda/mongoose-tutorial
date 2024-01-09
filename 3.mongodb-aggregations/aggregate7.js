b5f7f5125f07082ba008dbf73e04871332d21505
PuneetSivananda <puneet.sivananda@gmail.com>
2024-01-09 11:22:24 +0530

    feat: types of count stage

--- 3.mongodb-aggregations/aggregate7.js
+++ 3.mongodb-aggregations/aggregate7.js
@@ -13,10 +13,16 @@
 // Count Stage
 console.log("=============Results=start===============");
 
-const results = db.persons.aggregate([]).toArray();
+const results = db.persons.aggregate([]).toArray(); //slowest
 const results1 = db.persons.aggregate([]).itcount();
+const results2 = db.persons.aggregate([
+  {
+    $count: "total",
+  },
+]); //fastest
 
 console.log(results.length);
 console.log(results1);
+console.log(results2);
 
 console.log("=============Results=End=================");
