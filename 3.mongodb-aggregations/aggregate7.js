ccb254d6198420fcaf8b857ca75120b7118bafa7
PuneetSivananda <puneet.sivananda@gmail.com>
2024-01-09 11:13:22 +0530

    feat: count aggregation

--- 3.mongodb-aggregations/aggregate7.js
+++ 3.mongodb-aggregations/aggregate7.js
@@ -1,23 +1,21 @@
 /**
  * Aggregation Stages
  * -  match
- * -  group *
+ * -  group
  * -  project
  * -  sort
- * -  count
+ * -  count *
  * -  limit
  * -  skip
  * -  out
  */
 
-// Match and Group Stages
+// Count Stage
 console.log("=============Results=start===============");
 
 const results = db.persons.aggregate([
   // Stage 1
-  { $group: { _id: { age: "$age", eyeColor: "$eyeColor" } } },
-  // Stage 2
-  { $match: { "_id.age": { $gt: 30 } } },
+  { $count: "allDocumentsCount" },
 ]);
 
 console.log(results);
