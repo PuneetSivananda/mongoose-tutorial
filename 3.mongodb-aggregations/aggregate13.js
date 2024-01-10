0c7acf9325eabb815a7a2ecfbc6fe550fa4e5673
PuneetSivananda <puneet.sivananda@gmail.com>
2024-01-10 17:37:27 +0530

    wip: group stage sample code

---
+++ 3.mongodb-aggregations/aggregate13.js
@@ -0,0 +1,38 @@
+/**
+ * Aggregation Stages
+ * -  match
+ * -  group *
+ * -  project 
+ * -  sort
+ * -  count
+ * -  limit
+ * -  skip
+ * -  out
+ */
+
+// Group Stage
+console.log("=============Results=start===============");
+
+const results = db.persons.aggregate([
+  //Stage 1
+  {
+    $project: {
+      _id: 0,
+      index: 1,
+      name: 1,
+      info:{
+        eyes: "$eyeColor",
+        company: "$company.title",
+        country: "$company.location.country"
+      }
+    },
+  },
+  //Stage 2
+  {
+    $limit: 5,
+  },
+]);
+
+console.log(results);
+
+console.log("=============Results=End=================");
