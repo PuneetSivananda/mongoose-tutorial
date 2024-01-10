f0288ddd81b2be71d03ba41f35971a2c0c6b4bae
PuneetSivananda <puneet.sivananda@gmail.com>
2024-01-10 18:05:14 +0530

    feat: updates

--- 3.mongodb-aggregations/aggregate14.js
+++ 3.mongodb-aggregations/aggregate14.js
@@ -17,20 +17,9 @@ console.log("=============Results=start===============");
 
 const results = db.persons.aggregate([
   {
-    $project: {
-      _id: 0,
-      index: 1,
-      name: 1,
-      info: {
-        eyeColor: "$eyeColor",
-        company: "$company.title",
-        country: "$company.location.country",
-      },
-    },
-  },
-  {
     $group: {
-      _id: "$info.eyeColor",
+      _id: "$age",
+      count: { $sum: 1 },
     },
   },
 ]);
