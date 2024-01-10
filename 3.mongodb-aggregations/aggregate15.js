f5e229eeda5313634cd7cb83093f252f5990f80b
PuneetSivananda <puneet.sivananda@gmail.com>
2024-01-10 18:07:53 +0530

    wip: group and unwind stages

---
+++ 3.mongodb-aggregations/aggregate15.js
@@ -0,0 +1,29 @@
+/**
+ * Aggregation Stages
+ * -  match
+ * -  group *
+ * -  project
+ * -  sort
+ * -  count
+ * -  limit
+ * -  skip
+ * -  out
+ * -  unwind
+ * -  sum *
+ */
+
+// Accumulators Stage
+console.log("=============Results=start===============");
+
+const results = db.persons.aggregate([
+  {
+    $group: {
+      _id: "$favoriteFruit",
+      count: { $sum: 1 },
+    },
+  },
+]);
+
+console.log(results);
+
+console.log("=============Results=End=================");
